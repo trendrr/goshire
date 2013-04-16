83bc4785cde9be7bac74fa6a31b2e261f7c3363d
Dustin Norlander <dustinn@gmail.com>
2013-04-16 16:36:48 -0700

    clean up websocket writer and controller

--- cheshire/websocketlistener.go
+++ cheshire/websocketlistener.go
@@ -3,7 +3,6 @@ package cheshire
 import (
 	"bufio"
 	"code.google.com/p/go.net/websocket"
-	// "github.com/trendrr/cheshire-golang/dynmap"
 	"encoding/json"
 	"io"
 	"log"
@@ -11,21 +10,21 @@ import (
 	"sync"
 )
 
-type WebsocketConnection struct {
+type WebsocketWriter struct {
 	conn       *websocket.Conn
 	writerLock sync.Mutex
 }
 
-func (conn WebsocketConnection) Write(response *Response) (int, error) {
+func (this *WebsocketWriter) Write(response *Response) (int, error) {
 	json, err := json.Marshal(response)
 	if err != nil {
 		//TODO: uhh, do something..
 		log.Print(err)
 	}
 	// log.Println("writing ", string(json))
-	defer conn.writerLock.Unlock()
-	conn.writerLock.Lock()
-	bytes, err := conn.conn.Write(json)
+	defer this.writerLock.Unlock()
+	this.writerLock.Lock()
+	bytes, err := this.conn.Write(json)
 	return bytes, err
 }
 
@@ -51,11 +50,11 @@ func NewWebsocketController(route string, config *ServerConfig) WebsocketControl
 }
 
 // implements the HttpHijacker interface so we can handle the request directly.
-func (this WebsocketController) HttpHijack(writer http.ResponseWriter, req *http.Request) {
+func (this *WebsocketController) HttpHijack(writer http.ResponseWriter, req *http.Request) {
 	this.Handler.ServeHTTP(writer, req)
 }
 
-func (wc WebsocketController) HandleWCConnection(ws *websocket.Conn) {
+func (this *WebsocketController) HandleWCConnection(ws *websocket.Conn) {
 	// Uhh, guessing we are already in a go routine..
 	log.Print("CONNECT!")
 
@@ -64,7 +63,7 @@ func (wc WebsocketController) HandleWCConnection(ws *websocket.Conn) {
 	// conn.writer = bufio.NewWriter(conn.conn)
 
 	dec := json.NewDecoder(bufio.NewReader(ws))
-	conn := WebsocketConnection{conn: ws}
+	writer := &WebsocketWriter{conn: ws}
 	for {
 		var req Request
 		err := dec.Decode(&req)
@@ -78,12 +77,12 @@ func (wc WebsocketController) HandleWCConnection(ws *websocket.Conn) {
 		}
 		log.Print(req)
 		log.Print(req.Uri())
-		controller := wc.serverConfig.Router.Match(req.Method(), req.Uri())
+		controller := this.serverConfig.Router.Match(req.Method(), req.Uri())
 
 		log.Print("GOT CONTROLLER ")
 		log.Print(controller)
 
-		go controller.HandleRequest(&req, conn)
+		go controller.HandleRequest(&req, writer)
 	}
 	log.Print("DISCONNECT!")
 }
