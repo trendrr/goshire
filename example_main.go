a16dec126c35c6f35961ef904f586aaa61b8f174
Dustin Norlander <dustinn@gmail.com>
2013-04-16 15:32:04 -0700

    rename for connection

--- example_main.go
+++ example_main.go
@@ -10,7 +10,7 @@ func main() {
     bootstrap := cheshire.NewBootstrapFile("example_config.yaml")
     log.Println("HERE:1")
     //a ping controller api controller.  
-    pinger := func(request *cheshire.Request,conn cheshire.Connection) {
+    pinger := func(request *cheshire.Request,conn cheshire.Writer) {
         // log.Printf("PING! %s", request.Strest.Params)
         response := request.NewResponse()
         response.Put("data", "PONG")
@@ -22,7 +22,7 @@ func main() {
     log.Println("HERE:1")
     
     //an example html page
-    four04 := func(request *cheshire.Request, conn *cheshire.HtmlConnection) {
+    four04 := func(request *cheshire.Request, conn *cheshire.HtmlWriter) {
         context := make(map[string]interface{})
         context["message"] = "this is a 404 page"
         conn.Render("/404.html", context)
@@ -30,7 +30,7 @@ func main() {
     cheshire.RegisterHtml("/404", "GET", four04)
 
     //an example redirect page
-    redirect := func(request *cheshire.Request, conn *cheshire.HtmlConnection) {
+    redirect := func(request *cheshire.Request, conn *cheshire.HtmlWriter) {
         conn.Redirect("/ping")
     }
     cheshire.RegisterHtml("/redirect", "GET", redirect)
