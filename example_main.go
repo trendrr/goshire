e793d578998f2e74775865a36134c6d445ee148a
Dustin Norlander <dustinn@gmail.com>
2013-04-17 14:37:28 -0700

    add example filters

--- example_main.go
+++ example_main.go
@@ -5,9 +5,31 @@ import (
     "github.com/trendrr/cheshire-golang/cheshire"
 )
 
+type DummyFilter struct {
+    filterName string
+}
+func (this *DummyFilter) Before(*cheshire.Txn) bool {
+    log.Printf("BEFORE! (%s) \n", this.filterName)
+    return true
+}
+func (this *DummyFilter) After(*cheshire.Response, *cheshire.Txn) {
+    log.Printf("AFTER! (%s) \n", this.filterName)   
+}
+
 func main() {
 
+    //create a couple dummy filters
+
+    //this one will get executed on every request.
+    globalFilter := &DummyFilter{"global"}
+    //this one will only get executed on ping requests.
+    pingFilter := &DummyFilter{"ping"}
+
     bootstrap := cheshire.NewBootstrapFile("example_config.yaml")
+
+    bootstrap.AddFilters(globalFilter)
+
+
     log.Println("HERE:1")
     //a ping controller api controller.  
     pinger := func(txn *cheshire.Txn) {
@@ -18,7 +40,7 @@ func main() {
         txn.Write(response)
     }
     //now register the api call
-    cheshire.RegisterApi("/ping", "GET", pinger)
+    cheshire.RegisterApi("/ping", "GET", pinger, pingFilter)
     log.Println("HERE:1")
     
     //an example html page
