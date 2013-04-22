a03997d92a393ad20210bb644bc087ee692fc023
Dustin Norlander <dustinn@gmail.com>
2013-04-22 13:28:46 -0700

    add a multiplex event object

--- cheshire/helpers.go
+++ cheshire/helpers.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	// "strconv"
 	// "time"
+	"sync"
 )
 
 // Sends an error response to the channel
@@ -41,4 +42,73 @@ func RandString(length int) string {
 	str := strings.TrimRight(
 		base32.StdEncoding.EncodeToString(k), "=")
 	return str[:length]
+}
+
+// Simple multiplex event handler
+type Events struct {
+	inchan chan Event
+	outchans []chan Event
+	lock sync.Mutex
+}
+
+type Event struct {
+	eventType string
+	eventMessage string
+}
+
+// Creates a new Events object
+func NewEvents() *Events{
+	events := &Events{
+		inchan := make(chan Event, 10),
+		outchans := make([]chan Event, 0),
+	}
+	go func(events *Events) {
+		for {
+			e := <- events.inchan
+			events.lock.Lock()
+			for _, out := range(events.outchans) {
+				go func(event Event) {
+					select {
+						case err := out <- event:
+							if err != nil {
+								//remove this channel
+								event.remove(out)
+							}
+						default: //the event skips if the channel is unavail. 
+					}
+				}(e)
+			}
+			events.lock.Unlock()
+		}
+	}(events)	
+	return events
+}
+
+// Emits this message to all the listening channels
+func (this *Events) Emit(eventType, eventMessage string) {
+	inchan <- Event{eventType: eventType, eventMessage:eventMessage}
+}
+
+func (this *Events) Listen(eventchan chan Event) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	this.outchans = append(this.outchans, eventchan)
+}
+
+func (this *Events) Unlisten(eventchan chan Event) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	this.remove(eventchan)
+}
+
+func (this *Events) remove(eventchan chan Event) {
+	ch := make([]chan Event, 0)
+	for _, c := range(this.outchans) {
+		if c != eventType {
+			ch = append(ch, c)
+		} else {
+			log.Println("Found channel, removing...")
+		}
+	}
+	this.outchans = ch
 }
